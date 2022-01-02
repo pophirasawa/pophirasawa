ec92e8e97857e706380c502f21b90bb613df6d49
pophirasawa <81603561+pophirasawa@users.noreply.github.com>
2022-01-02 12:54:46 +0800

    TEST

--- deployment.bat
+++ deployment.bat
@@ -1,1 +1,1 @@
-hexo clean&&hexo g&&hexo --config _config_wiki.yml clean&&hexo --config _config_wiki.yml g&&rm db.json
+hexo clean&&hexo g&&hexo --config _config_wiki.yml clean&&hexo --config _config_wiki.yml g&&DEL db.json
