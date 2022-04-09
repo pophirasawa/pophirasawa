964f6769b8679651fc41fcc5699edc24b7fdc1ef
pophirasawa <81603561+pophirasawa@users.noreply.github.com>
2022-04-10 04:13:20 +0800

    1

--- deployment.bat
+++ deployment.bat
@@ -1,1 +1,2 @@
-DEL db.json&&hexo clean&&hexo g&&hexo --config _config_wiki.yml clean&&hexo --config _config_wiki.yml g&&DEL db.json
+hexo clean&&hexo g&&hexo --config _config_wiki.yml clean&&hexo --config _config_wiki.yml g&&DEL db.json
+echo 按任意键退出 & pause
