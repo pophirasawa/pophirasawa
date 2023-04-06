8726c4c9dcd4e7bc381b02c95fb0fbc69de8af63
pophirasawa <81603561+pophirasawa@users.noreply.github.com>
2023-04-07 03:15:00 +0800

    fix cdn
    fix cdn

--- deployment.bat
+++ deployment.bat
@@ -1,2 +1,2 @@
 hexo clean&&hexo g&&hexo --config _config_wiki.yml clean&&hexo --config _config_wiki.yml g&&DEL db.json
-echo 按任意键退出 & pause
+pause
---
+++ source_wiki/_posts/aaa.md
@@ -0,0 +1,12 @@
+---
+title: aaa
+toc: true
+date: 2023-04-07 01:49:01
+tags:
+categories:
+---
+
+
+
+
+
